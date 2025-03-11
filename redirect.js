b8d1289cf8c691a34652debee0ac425ca5887924
nizamike229 <nizamike229@mail.ru>
2025-03-11 20:00:36 +0500

    new

--- redirect.js
+++ redirect.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const whatsappUrl = `https://api.whatsapp.com/send?phone=${window.env.NUM}&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9C%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D0%B2%D0%B0%D1%88%D0%B0%20%D0%BF%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%86%D0%B8%D1%8F`;
-    const name = window.env.NAME;
-    const phone = window.env.PHONE;
+    const whatsappUrl = "https://api.whatsapp.com/send?phone=__NUM__&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9C%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D0%B2%D0%B0%D1%88%D0%B0%20%D0%BF%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%86%D0%B8%D1%8F";
+    const name = '__NAME__';
+    const phone = '__NUM__';
     const mainPageUrl = "https://minternational.ru/kz";
     const myDomain = "src2.pages.dev";
     const returnUrl = "https://" + myDomain;
